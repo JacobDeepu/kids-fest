e65ba1544528bd6a1aa7df5542abd96102882a31
Jacob Deepu <d2jdeepu@gmail.com>
2022-09-21 09:32:03 +0530

    Add Seeder to create admin permissions

---
+++ database/seeders/AdminPermissionSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class AdminPermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $permissions = [
+            'section list',
+            'section create',
+            'section edit',
+            'section delete',
+            'event list',
+            'event create',
+            'event edit',
+            'event delete'
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+        $role = Role::findByName('Admin');
+        $role->givePermissionTo($permissions);
+    }
+}
