fa72004b8a3ed1560395787d92ae0cbf54082ce8
Jacob Deepu <d2jdeepu@gmail.com>
2022-10-05 09:17:54 +0530

    Redesign welcome page

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,9 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            backgroundImage: {
+                'hero-bg': "url('../../images/hero-bg.jpg')",
+            }
         },
     },
 
